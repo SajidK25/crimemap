c6c7d8aae060b9afb4bfe92fd61d0023d6d2b185
Sajidk25 <sajidk25@gmail.com>
2016-10-19 04:28:00 +0600

    add new crimes functionality

--- crimemap.py
+++ crimemap.py
@@ -1,4 +1,5 @@
 from flask import Flask,request,render_template,url_for
+import json
 import dbconfig
 if dbconfig.test:
     from mockdbhelper import MockDBHelper as DBhelper
@@ -8,32 +9,23 @@ else:
 app=Flask(__name__)
 DB=DBhelper()
 
-@app.route('/')
+@app.route("/")
 def home():
-    try:
-        data=DB.get_all_inputs()
-    except Exception as e:
-        print e
-        data=None
-    return render_template('home.html',data=data)
+    crimes = DB.get_all_crimes()
+    crimes = json.dumps(crimes)
+    return render_template("home.html", crimes=crimes)
 
-@app.route('/add',methods=['POST'])
-def add():
-    try:
-        data=request.form.get("user_input")
-        DB.add_input(data)
-    except Exception as e:
-        print e
+@app.route("/submitcrime", methods=['POST'])
+def submitcrime():
+    category = request.form.get("category")
+    date = request.form.get("date")
+    latitude = float(request.form.get("latitude"))
+    longitude = float(request.form.get("longitude"))
+    description = request.form.get("description")
+    DB.add_crime(category, date, latitude, longitude, description)
     return home()
 
-@app.route('/clear')
-def clear():
-    try:
 
-        DB.clear_all()
-    except Exception as e:
-        print e
-    return home()
 
 if __name__=='__main__':
     app.debug=True
---
+++ mockdbhelper.py
@@ -0,0 +1,20 @@
+class MockDBHelper(object):
+    """docstring for Mockdbhelper."""
+    def connect(self, database="crimemap"):
+        pass
+
+    def get_all_inputs(self):
+        return []
+
+    def add_input(self,category, date, latitude, longitude,description):
+        pass
+
+    def clear_all(self):
+        pass
+        
+    def get_all_crimes(self):
+    return [{ 'latitude': -33.301304,
+       'longitude': 26.523355,
+       'date': "2000-01-01",
+       'category': "mugging",
+       'description': "mock description" }]
